c20c52795c6dba984e09718afe92c91a59e047bd
Michael Jones <michael.jones@evolution-apps.co.uk>
2025-06-04 20:51:15 +0100

    add data for liverpool and portsmouth

--- src/main.py
+++ src/main.py
@@ -21,16 +21,22 @@ analysis_service = AnalysisService()
 
 # model = OllamaModel("llama3.1:8b", temperature=0.7, top_p=0.85, top_k=100)
 load_dotenv("secrets.env")
-model = OpenAIModel(model_name="gpt-4o-mini", api_key=os.getenv("OPENAI_API_KEY"), temperature=0.7, top_p=0.85, top_k=100)
+model = OpenAIModel(
+    model_name="gpt-4o-mini",
+    api_key=os.getenv("OPENAI_API_KEY"),
+    temperature=0.7,
+    top_p=0.85,
+    top_k=100,
+)
 location = "Newcastle, UK"
 region = "E12000001"
 n_households = 300
 batch_size = 10
-include_stats = False
-include_target = True
-include_guidance = True
-compute_household_size = False
-use_microdata = True
+include_stats = True
+include_target = False
+include_guidance = False
+compute_household_size = True
+use_microdata = False
 no_occupation = False
 
 if use_microdata:
@@ -42,7 +48,9 @@ elif no_occupation:
 else:
     prompt_file = "minimal_prompt_2.txt"
 
-prompt = file_service.load_prompt(prompt_file, {"LOCATION": location, "TOTAL_HOUSEHOLDS": str(n_households)})
+prompt = file_service.load_prompt(
+    prompt_file, {"LOCATION": location, "TOTAL_HOUSEHOLDS": str(n_households)}
+)
 
 if no_occupation:
     schema = file_service.load_schema("household_schema_no_occupation.json")
@@ -54,7 +62,21 @@ population_id = str(uuid.uuid4())
 start_time = time.time()
 
 try:
-    households = population_service.generate_households(n_households, model, prompt, schema, location, region, batch_size, include_stats, include_guidance, use_microdata, compute_household_size, include_target, no_occupation)
+    households = population_service.generate_households(
+        n_households,
+        model,
+        prompt,
+        schema,
+        location,
+        region,
+        batch_size,
+        include_stats,
+        include_guidance,
+        use_microdata,
+        compute_household_size,
+        include_target,
+        no_occupation,
+    )
     execution_time = time.time() - start_time
 
     flat_data = [person for household in households for person in household]
